e090bb7763c3f01bea436d40146d2d6bfb62119b
Yi Angelov <mavisluan83@gmail.com>
2018-09-19 13:42:01 -0700

    update the app style

--- src/App.js
+++ src/App.js
@@ -4,28 +4,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <div className='display'>result</div>
-        <div className='input'>
-          <div className='left'>
-            <div style={{ width: '300px', height: '100px', color: 'gray'}}>clear</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-          </div>
-          <div className='right'>
-            <div><i className="fas fa-divide"></i></div>
-            <div><i className="fas fa-plus"></i></div>
-            <div><i className="fas fa-minus"></i></div>
-            <div><i className="fas fa-equals"></i></div>
-          </div>
-        </div>
+      <div className='grid-container'>
+        <span className='result'>result</span>
+        <div className='clear'>clear</div>
+        <div className='operator'><i className="fas fa-divide"></i></div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div className='operator'><i className="fas fa-plus"></i></div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div className='operator'><i className="fas fa-minus"></i></div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div> 
+        <div className='operator'><i className="fas fa-equals"></i></div>
       </div>
     );
   }
